a0214378fb7c52d4cf8a1ff85e4658f9f9bdf864
leep18 <leep18@bitbucket.org>
2023-01-15 17:15:24 -0500

    added stuff

---
+++ single_linkedlist.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next    
+def mergeTwoLists(list1, list2):
+    if not list1:
+        return head2
+    
+def create 
+    
+a = ListNode()
+
+
+b = ListNode()
+
+print(mergeTwoLists(a,b))
