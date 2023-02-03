456c97b76020b00ad257a159de4a1b07916183c8
leep18 <leep18@bitbucket.org>
2023-02-02 20:31:42 -0500

    heaps update

--- heaps/heaps.py
+++ heaps/heaps.py
@@ -1,13 +1,107 @@
-#max/min heapify, pop, print visualizer, add new value+balance, heap search
+#max/min heapify, pop, add new value+balance, heap search
+import math
+from io import StringIO
+import heapq
+
 class heaptime:
-    def __init__(self,heap=None):
-        self.heap = []
+    def __init__(self, arr=[]):
+        self.heap = arr
 
-    def create_rough_heap(self,lis):
-        for x in lis:
-            self.heap.append(x)
-        return self.heap
+    def add(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+        parent = (current - 1) // 2
+        # Compare and swap with parent until the value is larger or it's at the root
+        while current > 0 and self.heap[current] > self.heap[parent]:
+            self.heap[current], self.heap[parent] = self.heap[parent], self.heap[current]
+            current = parent
+            parent = (current - 1) // 2
 
-#heaptime = heaptime()
-#heaptime.create_rough_heap([1,2,3,4,5,56,2])
-#print(heaptime.heap)
+    def remove(self):
+        if not self.heap:
+            return None
+        maximum = self.heap[0]
+        last_leaf = self.heap.pop()
+        if self.heap:
+            self.heap[0] = last_leaf
+            current = 0
+            left_child = current * 2 + 1
+            right_child = current * 2 + 2
+            # Compare and swap with larger child until the value is smaller or it's at a leaf node
+            while (left_child < len(self.heap) and last_leaf < self.heap[left_child]) or (right_child < len(self.heap) and last_leaf < self.heap[right_child]):
+                if left_child < len(self.heap) and (right_child >= len(self.heap) or self.heap[left_child] > self.heap[right_child]):
+                    self.heap[current], self.heap[left_child] = self.heap[left_child], self.heap[current]
+                    current = left_child
+                else:
+                    self.heap[current], self.heap[right_child] = self.heap[right_child], self.heap[current]
+                    current = right_child
+                left_child = current * 2 + 1
+                right_child = current * 2 + 2
+        return maximum
+
+    def visualize_heap(self, total_width=60, fill=' '):
+        output = StringIO()
+        last_row = -1
+        for i, n in enumerate(self.heap):
+            if i:
+                row = int(math.floor(math.log(i+1, 2)))
+            else:
+                row = 0
+            if row != last_row:
+                output.write('\n')
+            columns = 2**row
+            col_width = int(math.floor((total_width * 1.0) / columns))
+            output.write(str(n).center(col_width, fill))
+            last_row = row
+        print (output.getvalue())
+        print ('-' * total_width)
+        return
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]
+        heapify(arr, n, largest)
+
+def build_max_heap(arr):
+    n = len(arr)
+
+    for i in range(n // 2, -1, -1):
+        heapify(arr, n, i)
+    return arr
+
+x = [9,2,65,3,6,1,64]
+p=[]
+for i in range(len(x)):
+    p.append(-x[i])
+
+y = heaptime(build_max_heap(x))
+y.visualize_heap()
+print(x)
+
+
+b=heapq.heapify(p)
+print(p)
+a = heaptime(p)
+a.visualize_heap()
+
+
+"""
+print(heaptime.heap)
+heaptime.visualize_heap()
+#parent = i/2
+#left = i * 2
+#right= 2i + 1
+heaptime.max_heapify()
+print(heaptime.heap)
+heaptime.visualize_heap()
+"""
