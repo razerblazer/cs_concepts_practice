0b629d346f9d21a19fbf8c8d34a491fac897fb51
Philip Lee <philiphockey009@gmail.com>
2023-03-30 22:48:29 +0000

    updated c logic

--- sorting_algorithms/insertion_sort_c/insertionsort.c
+++ sorting_algorithms/insertion_sort_c/insertionsort.c
@@ -3,35 +3,38 @@
 #include <stdlib.h>
 
 //this function takes the inputted numbers passed to through the command line and returns the pointer to the array in heap 
-int *arrayhelperforreal(char *mystr) {
+int *arrayhelper(char *mystr) {
 	int *returnarray;
 	returnarray = (int*)malloc(sizeof(int));
 	int offset = 0, lastknown = 0, elements = 0;
 	char *strrep;
 	while (*(mystr+offset) != '\0') {
 		if (*(mystr+offset) == ',') {
-			strrep = (char*)calloc(offset-lastknown, sizeof(char));
+			returnarray = (int*)realloc(returnarray,sizeof(int)*(elements+1));
+			strrep = (char*)calloc(1,sizeof(char));
 			for (int i = 0; i < offset - lastknown; i++) {
 				*(strrep+i) = *(mystr+lastknown+i);
 			}
+			printf("The string stored by strrep is %s\n", strrep);
 			*(returnarray+elements) = atoi(strrep);
-			free(strrep);
-			lastknown = offset;
+			printf("int value %d\n", (atoi(strrep)));
+			lastknown = offset+1;
 			offset++;
 			elements++;
+			free(strrep);
 		} else {
-			printf("This is the current offset value %d\n", offset);
 			offset++;
 		}
 	}
-	return returnarray;
+	return returnarray; //return pointer to integer array in heap
 }
 
 int main(int argc, char *argv[]){
 	printf("print argument %s\n", argv[1]); //1,2,3,4
 	int *heaparray;
 
-	heaparray = arrayhelperforreal(argv[1]);
+	heaparray = arrayhelper(argv[1]);
 	printf("This is the first element in my array %d\n", *heaparray);
+	printf("This is an element that is out of range in my array %p\n",*(heaparray+343244));
 	return 1;
 }
