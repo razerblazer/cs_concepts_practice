1d2332119bcd42f18e311f3e3eff7773300cc334
Philip Lee <philiphockey009@gmail.com>
2023-04-02 04:19:32 +0000

    finalized logic?

--- sorting_algorithms/insertion_sort_c/insertionsort.c
+++ sorting_algorithms/insertion_sort_c/insertionsort.c
@@ -4,10 +4,13 @@
 
 
 //struct 
-
+struct myStruct {
+	int *pointer;
+	int size;
+};
 
 //this function takes the inputted numbers passed to through the command line and returns the pointer to the array in heap 
-int *arrayhelper(char *mystr) {
+struct myStruct arrayhelper(char *mystr) {
 	int *returnarray;
 	returnarray = (int*)malloc(sizeof(int));
 	int offset = 0, lastknown = 0, elements = 0;
@@ -18,6 +21,10 @@ int *arrayhelper(char *mystr) {
 			strrep = (char*)calloc(1,sizeof(char));
 			for (int i = 0; i < offset - lastknown; i++) {
 				*(strrep+i) = *(mystr+lastknown+i);
+			
+			}
+			if (*(mystr+offset) == '\0') {
+				break;
 			}
 			printf("The string stored by strrep is %s\n", strrep);
 			*(returnarray+elements) = atoi(strrep);
@@ -26,23 +33,25 @@ int *arrayhelper(char *mystr) {
 			offset++;
 			elements++;
 			free(strrep);
-			if (*(mystr+offset+1) == '\0') {
-				break;
-			}
+
 		} else {
 			offset++;
 		}
 	}
-	return returnarray; //return pointer to integer array in heap
+	struct myStruct s1;
+	s1.pointer = returnarray;
+	s1.size = elements+1;
+	return s1; //return pointer to integer array in heap
 }
 
 int main(int argc, char *argv[]){
 	printf("print argument %s\n", argv[1]); //1,2,3,4
-	int *heaparray;
+	struct myStruct myarray;
 
-	heaparray = arrayhelper(argv[1]);
-	printf("This is the first element in my array %d\n", *heaparray);
-	printf("This is an element that is out of range in my array %d\n",*(heaparray+1));
-	for (int i=0, i < sizeof(heaparray))
+	myarray = arrayhelper(argv[1]);
+	printf("This is the first element in my array %d\n", *(myarray.pointer));
+	printf("This is the size of the array %d\n", myarray.size);
+//	printf("This is an element that is out of range in my array %d\n",*(heaparray+1));
+//	for (int i=0, i < sizeof(heaparray))
 	return 1;
 }
