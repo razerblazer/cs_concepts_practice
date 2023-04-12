4370f18e65b460a40cb961c27314fb2851b68860
leep18 <leep18@bitbucket.org>
2023-04-12 01:59:32 -0400

    created huffman tree constructor function and random test case(to be removed later)

--- greedy_algorithms/Huffman_coding.py
+++ greedy_algorithms/Huffman_coding.py
@@ -1,9 +1,13 @@
-#create a max heap structure which depends on the number of occurances of a character in a string
 
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
 
 def sort_helper(entry):
     return entry[1]
-
+#using sort instead of building minheap(code generated is slightly different)
 def count_character_occurance(string):
     count = {}
     for char in string:
@@ -13,8 +17,45 @@ def count_character_occurance(string):
             count[char] = 1
     hold = []
     for element in count.keys():
-        hold.append((element, count[element]))
+        hold.append([element, count[element]])
     hold.sort(key=sort_helper)
     return hold
 
-print(count_character_occurance("absiasnsaijjjdwqkndqwnr"))
+
+
+def build_tree(sortedlist):
+    tree = Node([None,None])
+    while sortedlist != []:
+        if tree.left is None:
+            tree.left = sortedlist.pop(0)
+            continue
+        elif tree.right is None:
+            tree.right = sortedlist.pop(0)
+            tree.value[1] = tree.left[1]+tree.right[1]
+            continue
+        constructNode = Node([None, None])
+        if tree.value[1] < sortedlist[0][1]:
+            constructNode.left = tree
+            constructNode.right = sortedlist.pop(0)
+            constructNode.value[1] = constructNode.right[1] + constructNode.left.value[1]
+        else:
+            constructNode.right = tree
+            constructNode.left = sortedlist.pop(0)
+            constructNode.value[1] = constructNode.right.value[1] + constructNode.left[1]
+        tree = constructNode
+        
+    return tree
+            
+x = count_character_occurance("absiasnsaijjjdwqkndqwnr")
+print(x)
+y = build_tree(x)
+print(y)
+test = ""
+while True:
+    if not isinstance(y.right, Node):
+        test += "0"
+        break
+    test += "0"
+    y = y.right
+    
+print(test)
