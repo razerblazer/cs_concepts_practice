fe54d6c6d4e72c41c95e02b3d07b0c212ebc7ce2
leep18 <leep18@bitbucket.org>
2023-04-14 00:54:02 -0400

    added encode and decode function

--- greedy_algorithms/Huffman_coding.py
+++ greedy_algorithms/Huffman_coding.py
@@ -22,6 +22,7 @@ def count_character_occurance(string):
     hold.sort(key=sort_helper)
     return hold
 
+#building huffman tree structure to determine codes
 def build_tree(sortedlist):
     tree = Node([None,None])
     while sortedlist != []:
@@ -46,31 +47,53 @@ def build_tree(sortedlist):
             tree = constructNode
     return tree
 
-
+#generates a list of all huffman codes for each character in the string to be compressed
 def build_codes(root):
     codes = []
     def helper(node, current):
         if not node:
             return
         if not node.left and not node.right:
-            codes.append(current)
+            codes.append(current+[node.value[0]])
         else:
             helper(node.left, current + ["0"])
             helper(node.right, current + ["1"])
     helper(root, [])
     for x in range(len(codes)):
         codes[x] = ''.join(codes[x])
+        codes[x] = (codes[x][-1],codes[x][:-1]) 
     return codes
-        
-    
 
-x = count_character_occurance("bdddaaaaacccccc")
-
-y = build_tree(x)
-print(build_codes(y))
-test = ""
-while y.left:
-    test += "0"
-    y = y.left
+def encode(string):
+    character_occurance = count_character_occurance(string)
+    buffmantree = build_tree(character_occurance)
+    codelist = build_codes(buffmantree)
+    print(codelist)
+    codesdict = {} 
+    for code in codelist: #loading dictionary for faster code access
+        codesdict[code[0]] = code[1]
+    build_compressed_string = ""
+    for char in string:
+        build_compressed_string += codesdict[char]
+    return build_compressed_string
     
-print(test)
+print(encode("aaaaabccccccddd")) #compressed string for this case is 1111111111100000000101101101
+
+charcount = count_character_occurance("aaaaabccccccddd")
+tree = build_tree(charcount)
+def decode(encoded_str, tree_head):
+    nodetrack = tree_head
+    decoded_str = ""
+    index = 0
+    while len(encoded_str) > index:
+        if encoded_str[index] == "0":
+            nodetrack = nodetrack.left
+        else:
+            nodetrack = nodetrack.right
+        if nodetrack.value[0] is not None:
+            decoded_str += nodetrack.value[0]
+            nodetrack = tree_head
+        index += 1
+    return decoded_str
+
+print(decode("1111111111100000000101101101", tree))
